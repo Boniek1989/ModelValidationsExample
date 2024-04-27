c99d586524c7f1449618cb0e1024ee1b05f07b01
Your Name <you@example.com>
2024-04-27 10:47:04 +0200

    Add project files.

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationsExample.Models;
+
+namespace ModelValidationsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if (ModelState.IsValid==false)
+            {
+                string errors = string.Join("\n",
+                ModelState.Values.SelectMany(value => // outer loop
+                value.Errors).Select(err => // inter loop
+                err.ErrorMessage));
+                //foreach (var value in ModelState.Values)
+                //{ 
+                //    foreach (var error in value.Errors)
+                //    {
+                //        errorsList.Add(error.ErrorMessage);
+                //    }
+                //}
+                //string errors = string.Join("\n", errorsList); 
+                return BadRequest(errors);
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidationsExample/Models/Person.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ModelValidationsExample.CustomValidators;
+
+namespace ModelValidationsExample.Models
+
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "{0} is not valid")]
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} characters must be between {2} and {1} long")]
+        [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} characters must be an alphabet, space or dots")] // allows specified characters // "*" allows multiple characters
+        public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "{0} is not valid")]
+        [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
+        public string? email { get; set; }
+
+        [Phone(ErrorMessage = "{0) should be a proper phone number")]
+        [ValidateNever]
+        public string? phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password",ErrorMessage = "{0} and {1} does not match.")]
+        [Display(Name = "Re-enter password")]
+        public string? ConfirmPassword { get; set;}
+
+        [Range(0,999.99, ErrorMessage = "{0} should be bewtween ${1} and ${2}")] 
+        public double? Price { get; set; }
+        //[MinimumYearValidator(2005, ErrorMessage = "Date of birth should be newer than {0}")]
+        [MinimumYearValidator(2005)]
+        public DateTime? DateOfBirth { get; set; }
+
+        public DateTime? FromDate { get; set; }
+
+        [DateRangeValidator("FromDate", ErrorMessage = "'From Date' should be older than or equal to 'To date'")]
+        public DateTime? ToDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - Persone name: {PersonName}, email: {email}, phone: {phone}, password: {Password}, confirm password: {ConfirmPassword}, price: {Price}";
+        }
+    }
+}
